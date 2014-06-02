3c728a48bc44733dc227dac1159737b7dd05b970
Ed Watt <edwatt92@gmail.com>
2014-06-02 11:11:42 -0400

    interference avoidance enhancements from week 2

--- interference_avoidance.py
+++ interference_avoidance.py
@@ -11,12 +11,15 @@ from gnuradio import filter
 from gnuradio import fft
 from gnuradio import blocks
 from gnuradio import audio
+from gnuradio import eng_notation
 from optparse import OptionParser
 import sys
 import math
 import struct
 from gnuradio.eng_option import eng_option
 import threading
+import random
+from datetime import datetime
 
 MAX_RATE = 1000e6
 
@@ -82,14 +85,14 @@ class build_block(gr.top_block):
 		gr.top_block.__init__(self)
 
 		usage = "usage: %prog [options] min_freq max_freq"
-		parser = OptionsParser(option=eng_option, usage=usage)
-		parser.add_option("-a", "--args", type="string", default=""
+		parser = OptionParser(option_class=eng_option, usage=usage)
+		parser.add_option("-a", "--args", type="string", default="",
 						  help="UHD device address args [default=%default]")
 		parser.add_option("", "--spec", type="string", default=None,
 						  help="Subdevice of UHD device where appropriate")
-		parser.add_option("-R", "--rx-antenna", type="string", default=None,
+		parser.add_option("-R", "--rx-antenna", type="string", default="RX2",
 						  help="select RX antenna where appropriate")
-		parser.add_option("-T", "--tx-antenna", type="string", default=None,
+		parser.add_option("-T", "--tx-antenna", type="string", default="TX/RX",
 						  help="select TX antenna where appropriate")
 		parser.add_option("-s", "--samp-rate", type="eng_float", default=1e6,
 						  help="set sample rate [default=%default]")
@@ -115,14 +118,28 @@ class build_block(gr.top_block):
 			parser.print_help()
 			sys.exit(1)
 
-		self.channel_bandwidth = options.channel_bandwidth
+		self.channel_bandwidth = options.channel_bandwidth #fft channel bandwidth
 
 		self.min_freq = eng_notation.str_to_num(args[0])
 		self.max_freq = eng_notation.str_to_num(args[1])
 
+		if self.min_freq < 1e6: self.min_freq *= 1e6
+		if self.max_freq < 1e6: self.max_freq *= 1e6
+
+		self.n_channel_grps = 5 #number of channel groups
+		self.curr_channel_grp = 0
+		self.n_channels = 5 #number of channels / channel group		
+		self.curr_channel = 0
+
+		self.tx_guard_band = 4e6
+
+
 		if self.min_freq > self.max_freq:
 			#swap them
 			self.min_freq, self.max_freq = self.max_freq, self.min_freq
+
+		self.channel_grp_bw = (self.max_freq-self.min_freq) / self.n_channel_grps
+		self.tx_channel_bw = self.channel_grp_bw / self.n_channels # tx channel bw
 
 		if not options.real_time:
 			real_time = False
@@ -143,7 +160,7 @@ class build_block(gr.top_block):
 			self.u_rx.set_subdev_spec(options.spec, 0)
 
 		#Set the antenna
-		if options.antenna:
+		if options.rx_antenna:
 			self.u_rx.set_antenna(options.rx_antenna, 0)
 
 		self.u_rx.set_samp_rate(options.samp_rate)
@@ -152,9 +169,11 @@ class build_block(gr.top_block):
 		self.lo_offset = options.lo_offset
 
 		if options.fft_size is None:
-			self.fft_size = int(self.usrp_rate/self.channel_bandwidth)
-		else
+			self.fft_size = int(self.usrp_rate/self.channel_bandwidth) 
+		else:
 			self.fft_size = options.fft_size
+
+		print "FFT Size: %d, USRP Samp Rate: %d, Channel Bandwidth: %d" % (self.fft_size, self.usrp_rate, self.channel_bandwidth)	
 
 		self.squelch_threshold = options.squelch_threshold
 
@@ -169,13 +188,6 @@ class build_block(gr.top_block):
 
 		c2mag = blocks.complex_to_mag_squared(self.fft_size)
 
-		self.freq_step = self.nearest_freq((0.75 * self.usrp_rate), self.channel_bandwidth)
-		self.min_center_freq = self.min_freq + (self.freq_step / 2)
-		nsteps = math.ceil((self.max_freq - self.min_freq) / self.freq_step)
-		self.max_center_freq = self.min_center_freq + (nsteps * self.freq_step)
-
-		self.next_freq = self.min_center_freq
-
 		tune_delay = max(0, int(round(options.tune_delay * usrp_rate / self.fft_size))) # in fft_frames
 		dwell_delay = max(1, int(round(options.dwell_delay * usrp_rate / self.fft_size))) # in fft frames
 
@@ -186,6 +198,45 @@ class build_block(gr.top_block):
 
 		self.connect(self.u_rx, s2v, ffter, c2mag, stats)
 
+		#transmit chain
+
+
+		d = uhd.find_devices(uhd.device_addr(options.args))
+		if d:
+
+			uhd_type = d[0].get('type')
+			print "\nFound '%s'" % uhd_type
+		else:
+			print "\nNo device found"
+			self.u_tx = None
+			return
+
+		#check version of USRP and set num_channels
+
+		if uhd_type == "usrp":
+			tx_nchan = 2
+			rx_nchan = 2
+		else:
+			tx_nchan = 1
+			rx_nchan = 1
+
+		#setup transmit chain (usrp sink, signal source)
+
+		#usrp sink
+		stream_args = uhd.stream_args('fc32', channels = range(tx_nchan))
+		self.u_tx = uhd.usrp_sink(device_addr=options.args, stream_args=stream_args)
+		self.u_tx.set_samp_rate(self.usrp_rate)
+
+		if options.tx_antenna:
+			self.u_tx.set_antenna(options.tx_antenna,0)
+
+        #analog signal source - sig_source_c(sampling_freq,waveform, wave_freq, ampl, offset=0)
+		self.tx_src0 = analog.sig_source_c(self.u_tx.get_samp_rate(), analog.GR_CONST_WAVE, 0, 1.0, 0)
+	
+		#connect blocks
+
+		self.connect(self.tx_src0, self.u_tx)
+
 		if options.gain is None:
 			# if no gain was specified use the midpoint in dB
 			g = self.u_rx.get_gain_range()
@@ -194,19 +245,56 @@ class build_block(gr.top_block):
 		self.set_gain(options.gain)
 		print "gain =", options.gain
 
+		#initialize transmission parameters
+		self.set_channel_group(3)
+		self.step_count = 0
+
 	def set_next_freq(self):
 		target_freq = self.next_freq
 		self.next_freq = self.next_freq + self.freq_step
 		if self.next_freq >= self.max_center_freq:
 			self.next_freq = self.min_center_freq
+			# print "Step Count: %d" % self.step_count
+			# self.step_count = 0
+			print "Channel Group scan complete. Starting scan again."
+			sys.stdout.flush()
 
-		if not self.set_freq(target_freq):
+		if not self.set_rx_freq(target_freq):
 			print "Failed to set frequency to", target_freq
 			sys.exit(1)
 
 		return target_freq
 
-	def set_freq(self, target_freq):
+
+	def set_channel_group(self,channel_grp):
+		self.curr_channel_grp = channel_grp
+		#set new min and max freq for spectrum sense
+		self.spec_min_freq = self.min_freq + self.channel_grp_bw * self.curr_channel_grp
+		self.spec_max_freq = self.spec_min_freq + self.channel_grp_bw
+		#set initial tx freq
+		self.set_tx_channel(random.randint(0,(self.n_channels - 1)))
+		
+		self.update_spec_sense_parameters()
+
+	def update_spec_sense_parameters(self):
+		self.freq_step = self.nearest_freq((0.75 * self.usrp_rate), self.channel_bandwidth)
+		self.min_center_freq = self.spec_min_freq + (self.freq_step / 2)
+		print "Min center freq: %f" % self.min_center_freq
+		nsteps = math.ceil((self.spec_max_freq - self.spec_min_freq) / self.freq_step)
+		self.max_center_freq = self.min_center_freq + (nsteps * self.freq_step)
+		print "Max center freq: %f" % self.max_center_freq
+		self.next_freq = self.min_center_freq
+
+	def set_tx_channel(self, channel):
+		self.curr_channel = channel
+		target_freq = self.spec_min_freq + (self.curr_channel + 0.5) * self.tx_channel_bw
+		self.u_tx.set_center_freq(target_freq)	
+		self.tx_freq_min = target_freq - self.tx_guard_band
+		self.tx_freq_max = target_freq + self.tx_guard_band
+		print "\n TX Freq: %f" % target_freq
+
+
+	def set_rx_freq(self, target_freq):
 		"""
 		Set the cetner frequency we are interested in.
 
@@ -223,6 +311,7 @@ class build_block(gr.top_block):
 
 	def set_gain(self, gain):
 		self.u_rx.set_gain(gain)
+		self.u_tx.set_gain(gain)
 
 	def nearest_freq(self, freq, channel_bandwidth):
 		freq = round(freq / channel_bandwidth, 0) * channel_bandwidth
@@ -237,6 +326,9 @@ def main_loop(tb):
 	bin_start = int(tb.fft_size * ((1 - 0.75) / 2))
 	bin_stop = int(tb.fft_size - bin_start)
 
+	#create transmission hopping thread
+	#start thread
+
 	while 1:
 
 		# Get the next message sent from the C++ code (blocking call)
@@ -250,23 +342,30 @@ def main_loop(tb):
 
 		for i_bin in range(bin_start, bin_stop):
 			center_freq = m.center_freq
-			freq.bin_freq(i_bin, center_freq)
+			freq = bin_freq(i_bin, center_freq)
 			noise_floor_db = 10*math.log10(min(m.data)/tb.usrp_rate)
 			power_db = 10*math.log10(m.data[i_bin]/tb.usrp_rate) - noise_floor_db
+			tb.step_count += 1
+			# print datetime.now(), "center_freq", center_freq, "freq", freq, "power_db", power_db, "noise_floor_db", noise_floor_db
 
-			if (power_db > tb.squelch_threshold) and (freq >= tb.min_freq) and (freq <= tb.max_freq)
+			if (power_db > tb.squelch_threshold) and (freq >= tb.min_freq) and (freq <= tb.max_freq) and ((freq < tb.tx_freq_min) or (freq > tb.tx_freq_max)):
 				print datetime.now(), "center_freq", center_freq, "freq", freq, "power_db", power_db, "noise_floor_db", noise_floor_db
-
+				# stop transmission hopping
+				channel_grp_sel = range(tb.n_channel_grps)
+				channel_grp_sel.remove(tb.curr_channel_grp)
+				tb.set_channel_group(random.choice(channel_grp_sel))
+				break
+				# restart transmission hopping
 
 if __name__ == '__main__':
 	t = ThreadClass()
-    t.start()
+	t.start()
 
-    tb = build_block()
-    try:
-        tb.start()
-        main_loop(tb)
+	tb = build_block()
+	try:
+		tb.start()
+		main_loop(tb)
 
-    except KeyboardInterrupt:
-        pass
+	except KeyboardInterrupt:
+		pass
 
